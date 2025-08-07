39fcddd68603da1ccfa38f24972730ce0618ac8b
yuu0428 <@yurayurayuhra@icloud.com>
2025-08-08 03:17:38 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-4">
+      <button className="bg-blue-500 text-white px-4 py-2 rounded">
+        テストボタン
+      </button>
+    </div>
+  )
+}
